991bcaddb584a56802d3826b902c2df67abd4bce
Philippe Heymans Smith <pheymanss@gmail.com>
2020-09-13 00:12:26 -0600

    compile english and spanish versions

--- resume_compile.R
+++ resume_compile.R
@@ -4,8 +4,9 @@ install.packages('prettydoc')
 # .rmd compiler to PDF
 install.packages('webshot')
 
-#install phantomjs if not already installed
+#install phantomjs if not already installed (cannot run on Rstudio.cloud)
 webshot::install_phantomjs()
 
 # render file as pdf
-webshot::rmdshot("resume_philippe-heymans.rmd", "cv.pdf")
+webshot::rmdshot("english.rmd", "en.pdf")
+webshot::rmdshot("spanish.rmd", "es.pdf")
