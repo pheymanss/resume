026f59074696a5d0185254710bc442d188faa172
Philippe Heymans Smith <pheymanss@gmail.com>
2021-02-02 13:53:46 -0600

    Feb 2021 update
    New output format (rmdformats)
    New experience in Open Source Devlopment
    New rendering option pagedown::chrome_print()

--- resume_compile.R
+++ resume_compile.R
@@ -10,3 +10,7 @@ webshot::install_phantomjs()
 # render file as pdf
 webshot::rmdshot("english.rmd", "philippe_heymans_smith.pdf")
 webshot::rmdshot("spanish.rmd", "philippe_heymans_smith_es.pdf")
+
+# optionally, if you want it paginated and have google chrome installed
+pagedown::chrome_print(input = "english.rmd",
+                       output = "philippe_heymans_smith.pdf")
